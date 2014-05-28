dda254fcb1feb327f40befa25e0ecf8be8bd7096
alycda <alyda@me.com>
2014-05-28 12:51:32 -0700

    Grunt (githooks)

---
+++ grunt/githooks.js
@@ -0,0 +1,4 @@
+// https://github.com/firstandthird/load-grunt-config
+// https://github.com/rhumaric/grunt-githooks#the-githooks-task
+// http://viget.com/extend/grunt-getting-started-with-git-hooks
+// http://githooks.com/
