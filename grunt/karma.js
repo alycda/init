4367b0c62dab9fdbd6e76c3b75bdde54d236ef1d
alycda <alyda@me.com>
2014-05-28 12:49:00 -0700

    Grunt (Karma)

---
+++ grunt/karma.js
@@ -0,0 +1,3 @@
+// https://github.com/firstandthird/load-grunt-config
+// https://github.com/karma-runner/grunt-karma#config
+// http://karma-runner.github.io/0.10/intro/faq.html
--- grunt/phantomjs.js
+++ grunt/phantomjs.js
@@ -2,3 +2,4 @@
 // https://github.com/gruntjs/grunt-lib-phantomjs#an-inline-example
 // https://github.com/ariya/phantomjs/wiki/Screen-Capture
 // https://github.com/ariya/phantomjs/wiki/Related-Projects
+// http://daker.me/2013/07/testing-your-responsive-design-with-phantomjs.html
